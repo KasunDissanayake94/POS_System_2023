4200a7c9e6c4e551838a6a2d8a4a85da3cdbc5d4
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2023-02-19 20:23:07 +0530

    backend files updated

--- backend/pos/src/main/java/io/reflectoring/pos/model/Inventory.java
+++ backend/pos/src/main/java/io/reflectoring/pos/model/Inventory.java
@@ -1,43 +1,21 @@
-//package com.example.KS_POS.model;
-//
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//@Document
-//public class Inventory {
-//
-//    @Id
-//    private String inventoryId;
-//    private String inventoryName;
-//    private String inventoryCategory;
-//
-//    public Inventory(String inventoryId, String inventoryName, String inventoryCategory) {
-//        this.inventoryId = inventoryId;
-//        this.inventoryName = inventoryName;
-//        this.inventoryCategory = inventoryCategory;
-//    }
-//
-//    public String getInventoryId() {
-//        return inventoryId;
-//    }
-//
-//    public void setInventoryId(String inventoryId) {
-//        this.inventoryId = inventoryId;
-//    }
-//
-//    public String getInventoryName() {
-//        return inventoryName;
-//    }
-//
-//    public void setInventoryName(String inventoryName) {
-//        this.inventoryName = inventoryName;
-//    }
-//
-//    public String getInventoryCategory() {
-//        return inventoryCategory;
-//    }
-//
-//    public void setInventoryCategory(String inventoryCategory) {
-//        this.inventoryCategory = inventoryCategory;
-//    }
-//}
+package io.reflectoring.pos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    private String inventoryId;
+    private String inventoryName;
+    private String inventoryCategory;
+
+
+}
--- backend/pos/src/main/java/io/reflectoring/pos/service/InventoryService.java
+++ backend/pos/src/main/java/io/reflectoring/pos/service/InventoryService.java
@@ -1,88 +1,88 @@
-//package com.example.KS_POS.service;
-//
-//import com.example.KS_POS.model.Inventory;
-//import com.example.KS_POS.repository.InventoryRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class InventoryService {
-//    @Autowired
-//    private InventoryRepository inventoryRepository;
-//
-//    public ResponseEntity<Inventory> saveInventory(Inventory inventory){
-//        try {
-//            Inventory inventory1 = inventoryRepository.save(new Inventory(inventory.getInventoryId(), inventory.getInventoryName(), inventory.getInventoryCategory()));
-//            return new ResponseEntity<>(inventory1, HttpStatus.CREATED);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-//
-//    public ResponseEntity<List<Inventory>> getAllInventoriesByName(String name){
-//        try {
-//            List<Inventory> inventories = new ArrayList<Inventory>();
-//
-//            if (name == null)
-//                inventoryRepository.findAll().forEach(inventories::add);
-//            else
-//                inventoryRepository.findByInventoryNameContaining(name).forEach(inventories::add);
-//            if (inventories.isEmpty()) {
-//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//            }
-//            return new ResponseEntity<>(inventories, HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-//
-//    public ResponseEntity<Inventory> getInventoryById(String id){
-//        Optional<Inventory> inventoryData = inventoryRepository.findById(id);
-//
-//        if (inventoryData.isPresent()) {
-//            return new ResponseEntity<>(inventoryData.get(), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-//
-//    public ResponseEntity<Inventory> updateInventory(String id, Inventory inventory){
-//        Optional<Inventory> inventoryData = inventoryRepository.findById(id);
-//        if (inventoryData.isPresent()) {
-//            Inventory inventory1 = inventoryData.get();
-//            inventory1.setInventoryId(inventory.getInventoryId());
-//            inventory1.setInventoryName(inventory.getInventoryName());
-//            inventory1.setInventoryCategory(inventory.getInventoryCategory());
-//            return new ResponseEntity<>(inventoryRepository.save(inventory1), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//
-//    }
-//
-//    public ResponseEntity<HttpStatus> deleteInventory(String id){
-//        try {
-//            inventoryRepository.deleteById(id);
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//
-//    }
-//
-//    public ResponseEntity<HttpStatus> deleteAllInventories(){
-//        try {
-//            inventoryRepository.deleteAll();
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//
-//    }
-//}
+package io.reflectoring.pos.service;
+
+import io.reflectoring.pos.model.Inventory;
+import io.reflectoring.pos.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InventoryService {
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    public ResponseEntity<Inventory> saveInventory(Inventory inventory){
+        try {
+            Inventory inventory1 = inventoryRepository.save(new Inventory(inventory.getInventoryId(), inventory.getInventoryName(), inventory.getInventoryCategory()));
+            return new ResponseEntity<>(inventory1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Inventory>> getAllInventoriesByName(String name){
+        try {
+            List<Inventory> inventories = new ArrayList<Inventory>();
+
+            if (name == null)
+                inventoryRepository.findAll().forEach(inventories::add);
+            else
+                inventoryRepository.findByInventoryNameContaining(name).forEach(inventories::add);
+            if (inventories.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(inventories, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Inventory> getInventoryById(String id){
+        Optional<Inventory> inventoryData = inventoryRepository.findById(id);
+
+        if (inventoryData.isPresent()) {
+            return new ResponseEntity<>(inventoryData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<Inventory> updateInventory(String id, Inventory inventory){
+        Optional<Inventory> inventoryData = inventoryRepository.findById(id);
+        if (inventoryData.isPresent()) {
+            Inventory inventory1 = inventoryData.get();
+            inventory1.setInventoryId(inventory.getInventoryId());
+            inventory1.setInventoryName(inventory.getInventoryName());
+            inventory1.setInventoryCategory(inventory.getInventoryCategory());
+            return new ResponseEntity<>(inventoryRepository.save(inventory1), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    public ResponseEntity<HttpStatus> deleteInventory(String id){
+        try {
+            inventoryRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    public ResponseEntity<HttpStatus> deleteAllInventories(){
+        try {
+            inventoryRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
